cd09a706f8875855cd236399eddc96923d820df8
Brodde, Jan Patrick <jan-patrick.brodde@adesso.de>
2024-02-14 19:03:48 +0100

    init commit

---
+++ src/main/java/de/boargamemanager/boardgamemanager_backend/BoardgamemanagerBackendApplication.java
@@ -0,0 +1,14 @@
+package de.boargamemanager.boardgamemanager_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardgamemanagerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardgamemanagerBackendApplication.class, args);
+    }
+
+    private void doNothing(){};
+}
---
+++ src/test/java/de/boargamemanager/boardgamemanager_backend/BoardgamemanagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.boargamemanager.boardgamemanager_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardgamemanagerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
